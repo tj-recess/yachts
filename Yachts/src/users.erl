ec25f0a7a0438ffd222b804d625bf6e210c489a2
arpit1712 <arpit1712@c32c4616-6907-e41b-4ee6-8dd74e326fa2>
2010-11-19 12:43:55 +0000

    adding register method using DB, erroneous as of now. Need modification. Error - process_not_owner_of_odbc_connection

--- Yachts/src/users.erl
+++ Yachts/src/users.erl
@@ -16,7 +16,57 @@
 %% API Functions
 %%dwai, password, firstName, lastName, location, emailId
 
-initRegisteredUserList()->spawn(users, loginManager, [dict:new()]).
+initRegisteredUserList()->
+	application:start(odbc),
+	ConnString="Driver={MySQL ODBC 5.1 Driver};Server=localhost;Database=yachts;User=root;Password=root;Option=3;",
+	{ok, Conn}=odbc:connect(ConnString, []),
+	spawn(users, loginManagerDB, [dict:new(), Conn]).
+
+loginManagerDB(UserDict, Conn)->
+	receive
+		%%register a user
+		{From, register, {Username, Password, FirstName, LastName, Location, EmailId}}->
+			Result = odbc:param_query(Conn, "CALL RegisterUser(?,?,?,?,?,?)", 
+				[{{sql_varchar, 30},[Username]},
+				{{sql_varchar, 30},[Password]},
+				{{sql_varchar, 30},[FirstName]},
+				{{sql_varchar, 30},[LastName]},
+				{{sql_varchar, 30},[Location]},
+				{{sql_varchar, 30},[EmailId]}]
+				),
+			case Result of
+				{udpated, _ } ->
+					From ! success;
+			 	{error, _ } ->
+					From ! Result
+			end,
+			odbc:disconnect(Conn),
+			loginManagerDB(UserDict, Conn);
+		_ ->
+			odbc:disconnect(Conn),
+			loginManagerDB(UserDict, Conn)
+	end.
+		
+
+registerUser(LoginManagerPid, Username, Password, FirstName, LastName, Location, EmailId)
+  -> LoginManagerPid ! {self(), register, {Username, Password, FirstName, LastName, Location, EmailId}},
+	receive
+		success ->io:format("User is registered successfully!");
+		{error, Msg} ->Msg;
+		Other -> Other
+	end.
+
+loginUser(LoginManagerPid, Username, Password)->
+	LoginManagerPid ! {self() , login , Username, Password},
+	receive
+		true ->io:format("User is logged in successfully!");
+		false ->io:format("Username or password is incorrect, Login Failed!");
+		failure ->io:format("Username or password is incorrect, Login Failed!")
+	end.
+
+%%
+%% Local Functions
+%%
 
 loginManager(UserDict)->
 	receive
@@ -38,24 +88,3 @@ loginManager(UserDict)->
 						loginManager(UserDict)  
 			end
 	end.
-
-registerUser(LoginManagerPid, Username, Password, FirstName, LastName, Location, EmailId)
-  -> LoginManagerPid ! {self(), register, {Username, Password, FirstName, LastName, Location, EmailId}},
-	receive
-		success ->io:format("User is registered successfully!");
-		failure ->io:format("Username already exists, Registration Failed!")
-		
-	end.
-
-loginUser(LoginManagerPid, Username, Password)->
-	LoginManagerPid ! {self() , login , Username, Password},
-	receive
-		true ->io:format("User is logged in successfully!");
-		false ->io:format("Username or password is incorrect, Login Failed!");
-		failure ->io:format("Username or password is incorrect, Login Failed!")
-	end.
-
-%%
-%% Local Functions
-%%
-
