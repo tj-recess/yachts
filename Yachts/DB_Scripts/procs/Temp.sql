a77ad0cf1b36dd601d3417bec4f36fc0e8780712
arpit1712 <arpit1712@c32c4616-6907-e41b-4ee6-8dd74e326fa2>
2010-11-21 01:55:28 +0000

    Adding Temp script to verify operations.

---
+++ Yachts/DB_Scripts/procs/Temp.sql
@@ -0,0 +1,8 @@
+
+
+USE yachts;
+CALL RegisterUser('at','password', 'arpit', 'tripathi','gainesville','arpit1712@gmail.com');
+
+SELECT * FROM RegisteredUsers where Username = 'at';
+
+CALL GetUserInfo('at');
