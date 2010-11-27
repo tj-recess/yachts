e7292fccc4d2eacdae985b5fb1e1242b35fbc6c0
arpit1712 <arpit1712@c32c4616-6907-e41b-4ee6-8dd74e326fa2>
2010-11-27 14:02:48 +0000

    Minor Changes

--- Yachts/src/user.erl
+++ Yachts/src/user.erl
@@ -19,11 +19,14 @@
 handleClient(ClientSocket) ->
     case gen_tcp:recv(ClientSocket, 0) of
         {ok, Data} ->
+			io:format("user sent ~w", [Data]),
 			%%Parse the data first and take appropriate action
-            case parseClientMessage(self(),Data) of
+            case parseClientMessage(Data) of
 				{register,[Username, Password, FirstName, LastName, Location, EmailId]} ->
+					io:format("User sent : register"),
 					Status = userManager:registerUser(Username, Password, FirstName, LastName, Location, EmailId);
 				{login, Username, Password} ->
+					io:format("User sent : login"),
 					Result = userManager:loginUser(self(), Username, Password),
 					case Result of
 						{true, Reason} -> 
@@ -48,58 +51,75 @@ handleClient(ClientSocket) ->
 %% Local Functions
 %%
 
-parseClientMessage(CallerPid, Msg)->
+parseClientMessage(Msg)->
        [H|T]= string:tokens(Msg,"^"),
        case string:to_lower(H) of
        		"register" ->
-				CallerPid ! {register,T};
+				{register,T};
             "login" -> 
-				CallerPid ! {login,T};
+				{login,T};
 		   "createsession" ->
-			   	CallerPid ! {createSession, T};
+			   	{createSession, T};
 		   "addusertosession" ->
-			   CallerPid ! {addUserToSession, T};
+			   {addUserToSession, T};
 		   "chat" ->
-			   CallerPid ! {chat, T};
+			   {chat, T};
 		   "getallloggedinusers" ->
-			   CallerPid ! getAllLoggedInUsers
+			   getAllLoggedInUsers
        end.
 
 userLoop(ClientSocket) ->
-	case gen_tcp:recv(ClientSocket, 0) of
+	%%wait for 100 ms for user to send a command first,
+	case gen_tcp:recv(ClientSocket, 0, 100) of
     {ok, Data} ->
 		%%Parse the data first and take appropriate action
-            case parseClientMessage(self(),Data) of
+            case parseClientMessage(Data) of
 				{createSession, ListOfUsers} ->
-					Result = sessionManager:createSession(ListOfUsers),
-					case Result of
-						{true, Response} -> 
-							Status = string:join(["CreateSessionResponse"|["success"|Response]], "^");
-						{false,Reason} ->
-							Status = string:join(["CreateSessionResponse","failure",Reason], "^");
-						{timeout,Reason} -> 
-							Status = string:join(["CreateSessionResponse","failure",Reason], "^")
-					end;
+					sessionManager:createSession(ListOfUsers);
+					
 				{addUsersToSession, [SessionID|ListOfUsers]} ->
 					{IntSessionID, _} = string:to_integer(SessionID),
-					Result = sessionManager:addUsersToSession({IntSessionID, ListOfUsers}),
-					case Result of
-						{true, Response} -> 
-							Status = string:join(["CreateSessionResponse"|["success"|Response]], "^");
-						{false,Reason} ->
-							Status = string:join(["CreateSessionResponse"|["failure"|Reason]], "^");
-						{timeout,Reason} -> 
-							Status = string:join(["CreateSessionResponse"|["failure"|Reason]], "^")
-					end;
+					sessionManager:addUsersToSession({IntSessionID, ListOfUsers});
+
 				{chat, [Sender|[SessionID|Text]]}->
 					{IntSessionID, _} = string:to_integer(SessionID),
-					Status = sessionManager:chat({Sender, IntSessionID, string:join(Text,"^")});
+					sessionManager:chat({Sender, IntSessionID, string:join(Text,"^")});
+
 				getAllLoggedInUsers ->
 					LoggedInUsersList = users:getAllLoggedInUsers(),
-					Status = string:join(["LoggedInUsers"|LoggedInUsersList],"^")
-			end,
-			gen_tcp:send(ClientSocket, Status),
-            userLoop(ClientSocket);
-        {error, closed} ->
-            ok
-    end.
+					Status = string:join(["LoggedInUsers"|LoggedInUsersList],"^"),
+					gen_tcp:send(ClientSocket, Status)
+			end;
+			
+	{error, closed} ->
+    	ok
+			
+	end,	
+			
+		%%otherwise just wait for another 100 ms for other process to send some data	
+		receive
+			{createSessionResponse, success, Response} -> 
+				ResponseMsg = string:join(["CreateSessionResponse"|["success"|Response]], "^");
+			{createSessionResponse, failure,Reason} ->
+				ResponseMsg = string:join(["CreateSessionResponse","failure",Reason], "^");
+			{createSessionResponse, timeout,Reason} -> 
+				ResponseMsg = string:join(["CreateSessionResponse","timeout",Reason], "^");
+			{addUserToSessionResponse, success, Response} -> 
+				ResponseMsg = string:join(["addUserToSessionResponse"|["success"|Response]], "^");
+			{addUserToSessionResponse, failure,Reason} ->
+				ResponseMsg = string:join(["addUserToSessionResponse"|["failure"|Reason]], "^");
+			{addUserToSessionResponse, timeout,Reason} -> 
+				ResponseMsg = string:join(["addUserToSessionResponse"|["timeout"|Reason]], "^");
+			{chatResponse, success, Response} ->
+				ResponseMsg = string:join(["chatResponse"|["success"|Response]],"^");
+			{chatResponse, failure, Reason} ->
+				ResponseMsg = string:join(["chatResponse"|["failure"|Reason]],"^");
+			{chatResponse, timeout, Reason} ->
+				ResponseMsg = string:join(["chatResponse"|["timeout"|Reason]],"^")
+		after 100 ->
+			ResponseMsg = "",%%this will never be sent because we call the loop immediately
+			userLoop(ClientSocket)
+		end,
+
+	gen_tcp:send(ClientSocket, ResponseMsg),
+	userLoop(ClientSocket).
