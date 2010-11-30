c5ac2216027bed11d5b220b7ee80425e09fb02a2
arpit1712 <arpit1712@c32c4616-6907-e41b-4ee6-8dd74e326fa2>
2010-11-30 00:59:22 +0000

    server2 : resubmitted, couldn't submit last time.

---
+++ Yachts/src/server2.erl
@@ -0,0 +1,35 @@
+-module(server2).
+-compile(export_all).
+-import(user).
+
+-define(TCP_OPTIONS, [binary, { active, false}, { packet, 0 } , {reuseaddr, true}]).
+
+
+% Call echo:listen(Port) to start the service.
+listen(Port) ->
+    {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
+    accept(LSocket).
+
+% Wait for incoming connections and spawn the echo loop when we get one.
+accept(LSocket) ->
+    {ok, Socket} = gen_tcp:accept(LSocket),
+    spawn(fun() -> loop(Socket) end),
+    accept(LSocket).
+
+% Echo back whatever data we receive on Socket.
+loop(Socket) ->
+	user:dummy(Socket).
+%% 	user:handleClient(Socket).
+%%     case gen_tcp:recv(Socket, 0) of
+%%         {ok, Data} ->
+%%             gen_tcp:send(Socket, "loginResponse^success^Message"),
+%% 			gen_tcp:send(Socket,"another reponse"),
+%% 			user:handleClient(Socket),
+%%             loop(Socket);
+%%         {error, closed} ->
+%%             ok
+%%     end.
+
+%% handleClient(ClientSocket) ->
+%% 	gen_tcp:recv(ClientSocket, 0),
+%% 	gen_tcp:send(ClientSocket, "data from handle client").
