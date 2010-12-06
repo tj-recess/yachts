d7812fc0a668c31f55125c877a4a0206a4dd2c78
arpit1712 <arpit1712@c32c4616-6907-e41b-4ee6-8dd74e326fa2>
2010-12-06 10:03:38 +0000

    Used CopyOnWriteArrayList instead of checking concurrentModificationException

--- trunk/Jyachts/src/edu/ufl/java/SessionManager.java
+++ trunk/Jyachts/src/edu/ufl/java/SessionManager.java
@@ -2,6 +2,7 @@ package edu.ufl.java;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,14 +10,14 @@ import java.net.Socket;
 
 public class SessionManager 
 {
-	Map<String, ArrayList<String>> sessionMap = null;		//maintains a map of all users belonging to a particular session (denoted by SessionID)
+	Map<String, CopyOnWriteArrayList<String>> sessionMap = null;		//maintains a map of all users belonging to a particular session (denoted by SessionID)
 	private static SessionManager sessionManager = new SessionManager();	//singleton object of this class which is responsible to maintaining sessions globally
 	//initializing here itself to avoid double checked locking
 	private AtomicInteger sessionCount = new AtomicInteger(0);		//Atomic Integer value of sessionCount is used for creating new sessions as it is thread safe
 	
 	private SessionManager()
 	{
-		sessionMap = new ConcurrentHashMap<String, ArrayList<String>>();
+		sessionMap = new ConcurrentHashMap<String, CopyOnWriteArrayList<String>>();
 	}
 	
 	public static SessionManager getSessionManager()
@@ -30,10 +31,14 @@ public class SessionManager
 		//check if first user is logged in
 		
 		if(lm.getUserInfo(userNames.get(0)) == null)
-			return;	//invalid requrest, do nothing 
+		{//invalid requrest, send Bad query response to invoker
+			String responseMsg = "BadQuery^User need to login first";
+			writeOnUserSocket(responseMsg, userNames.get(0));
+			return;	 
+		}
 
 		int newSessionID = sessionCount.getAndIncrement();
-		sessionMap.put(Integer.toString(newSessionID), new ArrayList<String>());
+		sessionMap.put(Integer.toString(newSessionID), new CopyOnWriteArrayList<String>());
 		addUserToSessionHelper(Integer.toString(newSessionID), userNames);
 			
 	}
@@ -42,29 +47,22 @@ public class SessionManager
 	public void addUserToSessionHelper(String sessionID, ArrayList<String> userNames)
 	{
 		LoginManager lm = LoginManager.getLoginManager();
-				
+
 		for(String username:userNames)
 		{
-			ArrayList<String> existingUsersInSession = new ArrayList<String>();
-			int backoffValue = 1;
-			while(true)//optimistic concurrency control in exponential back off pattern
-			{
-				try{
-					existingUsersInSession.clear();//start over to collect all members from session if some new user is added
-					existingUsersInSession.addAll(sessionMap.get(sessionID));
-					break;//done with taking snapshot, concurrent execution successful
-				}
-				catch(ConcurrentModificationException cmex)
+			User sender = lm.getUserInfo(username);
+			if(sender != null)	//check if every user asked to be added to session is logged in or not
+			{					//if yes, add user to session otherwise just ignore
+				CopyOnWriteArrayList<String> existingUsersInSession = new CopyOnWriteArrayList<String>();
+				CopyOnWriteArrayList<String> list = sessionMap.get(sessionID);
+				synchronized (list)	//actual write operation has to be synchronized on the list of usernames stored in the hash map 
 				{
-					ConsoleLogger.log("Concurrent Modification Exception in SessionManager : " + cmex.toString());
-					backoffValue*=2;
-					try{Thread.sleep(backoffValue);}catch(InterruptedException iex){/*Can be spurious wake up, do nothing*/}
-				}
-			}
-			Socket aSocket = lm.getUserSocket(username);
-			if(aSocket != null && !aSocket.isClosed())
-			{
-				//user is eligible to b;e added
+					existingUsersInSession.addAll(list);
+					if(!list.contains(username))
+						list.add(username);
+					sessionMap.put(sessionID, list);	
+				}				
+				//user is added
 				//send response to existing users
 				String message= "addUsersToSessionResponse^success^" + sessionID;
 				String singleMessage= "addUsersToSessionResponse^success^" + sessionID + "^" + username;
@@ -72,10 +70,7 @@ public class SessionManager
 				for(String existingUser: existingUsersInSession){
 					writeOnUserSocket(singleMessage, existingUser);
 					message+="^"+existingUser;			
-				}
-				existingUsersInSession.add(username);
-				sessionMap.put(sessionID, existingUsersInSession);
-				
+				}				
 				//send response to newly added user				
 				writeOnUserSocket(message, username);
 			}
@@ -84,26 +79,16 @@ public class SessionManager
 	
 	public void removeUserFromSession(String sessionID, String username)
 	{
-		ArrayList<String> existingUsersInSession = new ArrayList<String>();//create a blank array list
-		int backoffValue = 1;
-		while(true)//optimistic concurrency control in exponential back off pattern
-		{
-			try{
-				existingUsersInSession.clear();//start over to collect all members from session if some new user is added
-				existingUsersInSession.addAll(sessionMap.get(sessionID)); //make a deep copy
-				break;//done with taking snapshot, concurrent execution successful
-			}
-			catch(ConcurrentModificationException cmex)
-			{
-				ConsoleLogger.log("Concurrent Modification Exception in SessionManager : " + cmex.toString());
-				backoffValue*=2;
-				try{Thread.sleep(backoffValue);}catch(InterruptedException iex){/*Can be spurious wake up, do nothing*/}
-			}
-		} 
+		CopyOnWriteArrayList<String> existingUsersInSession = new CopyOnWriteArrayList<String>();
+		CopyOnWriteArrayList<String> list = sessionMap.get(sessionID);
+		existingUsersInSession.addAll(list);
+		synchronized (list) {
+			list.remove(username);
+			sessionMap.put(sessionID, list);
+		}
 		if(existingUsersInSession.contains(username))
 		{
 			existingUsersInSession.remove(username);
-			sessionMap.put(sessionID, existingUsersInSession);
 			String response = "removeUserFromSessionResponse^success^" + sessionID + "^" + username;
 			for(String aUsername:existingUsersInSession)
 			{
@@ -119,22 +104,7 @@ public class SessionManager
 	
 	public void addUserToSession(String sessionID, ArrayList<String> userNames)
 	{
-		ArrayList<String> existingUsersInSession = new ArrayList<String>();
-		int backoffValue = 1;
-		while(true)//optimistic concurrency control in exponential back off pattern
-		{
-			try{
-				existingUsersInSession.clear();//start over to collect all members from session if some new user is added
-				existingUsersInSession.addAll(sessionMap.get(sessionID));
-				break;//done with taking snapshot, concurrent execution successful
-			}
-			catch(ConcurrentModificationException cmex)
-			{
-				ConsoleLogger.log("Concurrent Modification Exception in SessionManager : " + cmex.toString());
-				backoffValue*=2;
-				try{Thread.sleep(backoffValue);}catch(InterruptedException iex){/*Can be spurious wake up, do nothing*/}
-			}
-		}
+		CopyOnWriteArrayList<String> existingUsersInSession = sessionMap.get(sessionID);
 		if(existingUsersInSession != null && existingUsersInSession.contains(userNames.get(0)))
 		{
 			userNames.remove(0);
@@ -144,34 +114,8 @@ public class SessionManager
 	
 	public void chat(String username, String sessionID, String msg)
 	{
-		ArrayList<String> existingUsersInSession = new ArrayList<String>();//create a blank array list
-		int backoffValue = 1;
-		while(true)//optimistic concurrency control in exponential back off pattern
-		{
-			try{
-				existingUsersInSession.clear();//start over to collect all members from session if some new user is added
-				existingUsersInSession.addAll(sessionMap.get(sessionID)); //make a deep copy
-				break;//done with taking snapshot, concurrent execution successful
-			}
-			catch(ConcurrentModificationException cmex)
-			{
-				ConsoleLogger.log("Concurrent Modification Exception in SessionManager : " + cmex.toString());
-				backoffValue*=2;
-				try{Thread.sleep(backoffValue);}catch(InterruptedException iex){/*Can be spurious wake up, do nothing*/}
-			}
-		} 
-		/*This method uses Reentrant locks to avoid unintended message display to a user who was not present in chat 
-		 * at the time when message was sent but got added by some other user in the same session.
-		 * By acquiring a lock first
-
-		
-		ArrayList<String> existingUsersInSession = new ArrayList<String>();
-		try{
-			lock.lock();
-			existingUsersInSession.addAll(sessionMap.get(sessionID));//get the snapshot of users list
-		}
-		finally{lock.unlock();}		 
-		*/
+		CopyOnWriteArrayList<String> existingUsersInSession = sessionMap.get(sessionID);
+		//copy-on-write arraylist takes care of concurrent modification exceptions
 		if(existingUsersInSession.contains(username))
 		{
 			String response = "chatResponse^success^" + sessionID + ":" + username + ":" + msg;
