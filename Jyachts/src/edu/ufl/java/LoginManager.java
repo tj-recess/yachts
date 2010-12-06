4d5107254c2f8a840354fde5b51882b276906bc7
arpit1712 <arpit1712@c32c4616-6907-e41b-4ee6-8dd74e326fa2>
2010-12-06 06:01:27 +0000

    Cleaned up code. Still need to be commented.

--- Jyachts/src/edu/ufl/java/LoginManager.java
+++ Jyachts/src/edu/ufl/java/LoginManager.java
@@ -6,57 +6,59 @@ import java.util.Map;
 
 public class LoginManager {
         // keeps a track of currently users that are currently logged in to the chat server
-        //private static ArrayList<String> loggedInUsers = new ArrayList<String> ();  - need to store both username 
-		// and their ip/port config - so using hashmap instead
 	
         Map<String,User> loggedInUsersMap = null;
         Map<String,Socket> loggedInUsersSockets = null;
         
         /* the only instance of this class to avoid double checked locking*/
         private static LoginManager loginmgr = new LoginManager();
+        private DBManager dbm = null;
         
         // always returns the one instance 
-        public static synchronized LoginManager getLoginManager(){
+        public static LoginManager getLoginManager(){
         		return loginmgr;
         }
         
         private LoginManager(){
-        	loggedInUsersMap = new HashMap<String,User>();
-        	loggedInUsersSockets = new HashMap<String,Socket>(); 
+	        	loggedInUsersMap = new HashMap<String,User>();
+	        	loggedInUsersSockets = new HashMap<String,Socket>();
+	        	dbm = new DBManager();
         } // singleton
         
-        public boolean logoutUser(String username){
-        		System.out.println("LOGINMGR: Removing user from logged in users list "+username);
-        		User removedUser = loggedInUsersMap.remove(username); 
-        		loggedInUsersSockets.remove(username);
-        		System.out.println("LOGINMGR: User Removed: "+ removedUser.getUsername());
-        		return true;
+        public User logoutUser(String username)
+        {
+    		ConsoleLogger.log("LOGINMGR: Removing user from logged in users list "+username);
+    		User removedUser = loggedInUsersMap.remove(username); 
+    		loggedInUsersSockets.remove(username);
+    		ConsoleLogger.log("LOGINMGR: User Removed: "+ removedUser.getUsername());
+    		return removedUser;
         }
         
-        public boolean loginUser(String username, String password, Socket usersocket){
-                /* put the user in the logged in users list */
-        		System.out.println("LOGINMGR: Adding user to logged in users list: "+username);
-        		if(loggedInUsersMap.containsKey(username)){
-        			// user already logged in
-        			System.out.println("LOGINMGR: User already logged in.");
-        			return true;
-        		}
-        		else{
-        			DBManager dbm = new DBManager();
-        			if (dbm == null)
-        				return false;
-        			User loggedInUser = dbm.loginUser(username, password);
-        			if(loggedInUser != null)
-        			{
-        				// user added to list of logged in users.
-            			loggedInUsersMap.put(username,loggedInUser);
-            			loggedInUsersSockets.put(username, usersocket);
-                    	System.out.println("LOGINMGR: Added User: "+username+"\t Socket Details: "+ usersocket.toString());
-                    	return true;
-        			}
-        		}
-        		//control here means unsuccessful in logging in
-        		return false;
+        public boolean loginUser(String username, String password, Socket usersocket)
+        {
+            /* put the user in the logged in users list */
+    		ConsoleLogger.log("LOGINMGR: Adding user to logged in users list: "+username);
+    		if(loggedInUsersMap.containsKey(username)){
+    			// user already logged in
+    			ConsoleLogger.log("LOGINMGR: User already logged in.");
+    			return true;
+    		}
+    		else
+    		{
+    			if (dbm == null)
+    				return false;
+    			User loggedInUser = dbm.loginUser(username, password);
+    			if(loggedInUser != null)
+    			{
+    				// user added to list of logged in users.
+        			loggedInUsersMap.put(username,loggedInUser);
+        			loggedInUsersSockets.put(username, usersocket);
+                	ConsoleLogger.log("LOGINMGR: Added User: "+username+"\t Socket Details: "+ usersocket.toString());
+                	return true;
+    			}
+    		}
+    		//control here means unsuccessful in logging in
+    		return false;
         }
         
         public User getUserInfo(String username)
@@ -68,7 +70,7 @@ public class LoginManager {
         	return "LoggedInUsers^"+loggedInUsersMap.keySet().toString().replace('[', ' ').replace(']', ' ').replace(',','^').replaceAll(" ", "");
         }
         
-        public synchronized Socket getUserSocket(String username)
+        public Socket getUserSocket(String username)
         {
         	return loggedInUsersSockets.get(username);
         }
